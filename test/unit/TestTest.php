a0c55edd0a15ed87141b1387b4800127b7332195
Vidya Raghavan <vidya@shippable.com>
2014-08-07 11:22:01 +0530

    Update TestTest.php

--- test/unit/TestTest.php
+++ test/unit/TestTest.php
@@ -32,7 +32,7 @@ class TestTest extends TestCase
     public function testExecuteInvokesCallableWithArguments()
     {
         $fn = function ($a, $b) {
-            $this->assertEquals('foo', $a);
+            $this->assertEquals('foo1', $a);
             $this->assertEquals('bar', $b);
         };
 
